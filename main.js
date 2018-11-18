971bfb5be36df75d10281cdcc8ecb1a2b9468f93
Brian Herbert <briandangerous@gmail.com>
2018-11-18 14:36:03 +0000

    basic component

--- main.js
+++ main.js
@@ -1,13 +1,22 @@
+
+  Vue.component('todo-item', {
+    // The todo-item component now accepts a
+    // "prop", which is like a custom attribute.
+    // This prop is called todo.
+    props: ['todo'],
+    template: '<li>{{ todo.message }}</li>'
+  });
+
 var app = new Vue({
     el: '#app',
     data: {
       message: 'You loaded this page on ' + new Date().toLocaleString(),
       list: [
-          { message: "learn Vue basics" },
-          { message: "learn Vue CLI" },
-          { message: "build Folder size app with dotnet core backend" },
-          { message: "learn Elektron" },
-          { message: "build cross platform Folder Size app" },
+          { id: 0, message: "learn Vue basics" },
+          { id: 1, message: "learn Vue CLI" },
+          { id: 2, message: "build Folder size app with dotnet core backend" },
+          { id: 3, message: "learn Elektron" },
+          { id: 4, message: "build cross platform Folder Size app" },
       ],
       hello: 'hello world'
     },
@@ -15,14 +24,12 @@ var app = new Vue({
         reverseList: function() {
 
             let reversedList = [];
-            console.log(this.list.length);
             this.list.reverse().forEach(element => {
                 reversedList.push({
                     message: element.message
                 });
             });
             this.list = reversedList;
-            console.log(this.list.length);
         }
     }
   });

1f799ebcdc8dacad85c9bce1ba46a0825eafcf8b
Brian Herbert <brian.herbert@sabstt.com>
2018-11-19 17:32:10 +0000

    save backup

--- src/main.js
+++ src/main.js
@@ -1,21 +1,48 @@
-
 Vue.use(SemanticUIVue);
 var app = new Vue({
     el: '#app',
     data: {
-        list: [
-            {
-                id: 0, 
+        list: [{
+                id: 0,
                 message: "learn Vue basics",
-                child: [
-                    { id: 0, message: "declarations", child: [] },
-                    { id: 1, message: "interactions", child: [] }
+                child: [{
+                        id: 0,
+                        message: "declarations",
+                        child: []
+                    },
+                    {
+                        id: 1,
+                        message: "interactions",
+                        child: []
+                    }
                 ]
             },
-            { id: 1, message: "learn Vue CLI", child: [] },
-            { id: 2, message: "build Folder size app with dotnet core backend", child: [] },
-            { id: 3, message: "learn Elektron", child: [] },
-            { id: 4, message: "build cross platform Folder Size app", child: [] },
+            {
+                id: 1,
+                message: "learn Vue CLI",
+                child: []
+            },
+            {
+                id: 2,
+                message: "build Folder size app with dotnet core backend",
+                child: []
+            },
+            {
+                id: 3,
+                message: "learn Elektron",
+                child: []
+            },
+            {
+                id: 4,
+                message: "build cross platform Folder Size app",
+                child: []
+            },
         ]
+    },
+    methods: {
+        save: function () {
+            saveJSON(app.$data, 'backup.json');
+        }
     }
 });
+
